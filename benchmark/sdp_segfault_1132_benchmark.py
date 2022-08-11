25f6b8441a5a9e89373e2b72d8af9dd06913e80c
parthb83 <parthbansal1536@gmail.com>
2022-08-12 00:31:05 +0530

    Fixed isort issues

--- benchmark/sdp_segfault_1132_benchmark.py
+++ benchmark/sdp_segfault_1132_benchmark.py
@@ -11,9 +11,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import numpy as np
-
 import cvxpy as cp
+import numpy as np
 
 
 class SDPSegfault1132Benchmark():
